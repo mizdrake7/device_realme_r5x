8ae9da9875b054e44928960c8129a10e5edbda49
Sarthak Roy <sarthakroy2002@gmail.com>
2022-01-09 08:18:33 +0000

    r5x: radio: Inherit apn conf from RMX2050
    * From realme/RMX2050/RMX2050:11/RKQ1.200903.002/1623447963502:user/release-keys
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- device.mk
+++ device.mk
@@ -49,6 +49,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
