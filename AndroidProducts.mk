8793060a199e044a307fcd524523568a15655ce0
MAdMiZ <mizdrake7@gmail.com>
2022-04-09 10:27:19 +0000

    r5x: CorvusOS OFFICIAL!
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019 The VoltageOS Project
+# Copyright (C) 2022-2023 The CorvusOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/voltage_r5x.mk
+    $(LOCAL_DIR)/corvus_r5x.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_r5x-user \
-    voltage_r5x-userdebug \
-    voltage_r5x-eng
+    corvus_r5x-user \
+    corvus_r5x-userdebug \
+    corvus_r5x-eng
