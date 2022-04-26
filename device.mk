3191e309b0f3d9c47412972469483d27670f2e68
Lucchetto <lucchetto.tie@live.com>
2022-04-25 20:56:35 -0400

    r5x: configs create dummy libqti-perfd-client
    * This way, proprietary perfd blobs can be removed without breaking fingerprint
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -352,6 +352,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.r5x-libperfmgr \
     android.hardware.power.stats@1.0-service.pixel \
+    libqti-perfd-client
 
 # Perf
 PRODUCT_PACKAGES += \
