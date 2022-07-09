d89bbf5966caa55c7a2310486f31f6e7b0e6664c
MAdMiZ <mizdrake7@gmail.com>
2022-07-09 09:32:11 +0000

    r5x: Electrify OFFICIALLY!
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2022-2023 The VoltageeOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -116,6 +116,9 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     libhidltransport \
     libhidltransport.vendor
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1280
 
 # Camera
 PRODUCT_PACKAGES += \
