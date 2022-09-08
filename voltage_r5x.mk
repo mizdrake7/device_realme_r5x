35e0cae0de1974432253de9a8cefad16698aad96
MAdMiZ <mizdrake7@gmail.com>
2022-09-08 08:12:22 +0000

    r5x: Electrify OFFICIALLY!
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- voltage_r5x.mk
+++ voltage_r5x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023 The CorvusOS Project
+# Copyright (C) 2022-2023 The VoltageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common CorvusOS stuff
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+# Inherit some common Electrifying stuff
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -33,7 +33,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := corvus_r5x
+PRODUCT_NAME := voltage_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
@@ -45,9 +45,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
     PRIVATE_BUILD_DESC="unknown-user 10 QKQ1.200209.002 unknown release-keys"
 
-# CorvusOS
-RAVEN_LAIR=Official
-CORVUS_MAINTAINER=MAdMiZ
-USE_GAPPS=true
-USE_CUSTOM_CLANG=true
-TARGET_INCLUDE_PIXEL_CHARGER := true
+# VoltageOS
+VOLTAGE_BUILD_TYPE := OFFICIAL
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
