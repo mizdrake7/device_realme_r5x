04bd2c932f65fef00fef9a589a5634386307aeee
takumi021 <omrameshpatel@gmail.com>
2023-01-19 21:15:14 +0000

    r5x: configs: gps: Build Missing libs needed by GPS HAL
    03-24 13:29:51.704   450   487 E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library libloc_net_iface.so not found
    03-24 13:29:51.509   450   450 E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library liblocdiagiface.so not found
    
    Signed-off-by: takumi021 <omrameshpatel@gmail.com>

--- device.mk
+++ device.mk
@@ -240,23 +240,18 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor \
     libqti_vndfwk_detect.vendor
 
-# GNSS
+# GNSS/GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss.measurement_corrections@1.1.vendor
-
-# GPS
-PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss@2.1.vendor \
     android.hardware.gnss@2.1-service-qti \
-    android.hardware.gnss@2.1-impl-qti
-
-PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
     libbatching \
     libgeofencing \
-    libgnss
-
-PRODUCT_PACKAGES += \
+    libloc_net_iface \
+    liblocdiagiface \
+    libgnss \
     apdr.conf \
     flp.conf \
     gnss_antenna_info.conf \
@@ -264,9 +259,7 @@ PRODUCT_PACKAGES += \
     izat.conf \
     lowi.conf \
     sap.conf \
-    xtwifi.conf
-
-PRODUCT_PACKAGES += \
+    xtwifi.conf \
     gnss@2.0-base.policy \
     gnss@2.0-xtra-daemon.policy \
     gnss@2.0-xtwifi-client.policy \
