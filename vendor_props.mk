d81c958206269eb61288e9e4b6d13f56d062a09c
Alec Mouri <alecmouri@google.com>
2023-02-12 19:24:29 +0000

    r5x: props: Force device to treat 170M as sRGB in SF
    Bug: 229442032
    Test: builds, boots
    Change-Id: I2910f9d025ca6a6f3e73544e8f14346cfb0bc177
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -147,6 +147,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_client_composition_cache=1 \
     debug.sf.layer_caching_active_layer_timeout_ms=1000 \
     debug.sf.enable_gl_backpressure=1 \
+    debug.sf.treat_170m_as_sRGB=1 \
     debug.sf.predict_hwc_composition_strategy=0 \
     ro.opengles.version=196610 \
     ro.vendor.display.cabl=0 \
