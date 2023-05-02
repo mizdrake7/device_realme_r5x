5d9c6a559fbe7a93590104c90590f052af9b49ab
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2023-05-02 10:59:17 +0000

    r5x: power-libperfmgr: Remove dumpstate support
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- power-libperfmgr/aidl/Power.cpp
+++ power-libperfmgr/aidl/Power.cpp
@@ -23,7 +23,6 @@
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
-#include <android-base/stringprintf.h>
 #include <android-base/strings.h>
 
 #include <utils/Log.h>
@@ -195,27 +194,6 @@ ndk::ScopedAStatus Power::isBoostSupported(Boost type, bool *_aidl_return) {
     LOG(INFO) << "Power boost " << toString(type) << " isBoostSupported: " << supported;
     *_aidl_return = supported;
     return ndk::ScopedAStatus::ok();
-}
-
-constexpr const char *boolToString(bool b) {
-    return b ? "true" : "false";
-}
-
-binder_status_t Power::dump(int fd, const char **, uint32_t) {
-    std::string buf(::android::base::StringPrintf(
-            "HintManager Running: %s\n"
-            "SustainedPerformanceMode: %s\n"
-            "BatterySaverMode: %s\n",
-            boolToString(mHintManager->IsRunning()),
-            boolToString(mSustainedPerfModeOn),
-            boolToString(mBatterySaverOn)));
-    // Dump nodes through libperfmgr
-    mHintManager->DumpToFd(fd);
-    if (!::android::base::WriteStringToFd(buf, fd)) {
-        PLOG(ERROR) << "Failed to dump state to fd";
-    }
-    fsync(fd);
-    return STATUS_OK;
 }
 
 ndk::ScopedAStatus Power::createHintSession(int32_t tgid, int32_t uid,
