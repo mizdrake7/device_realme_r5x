37cf38b3ecab0f40f4016b75e4900f177af5406f
MadhavSaladi <baaswanthmadhav@gmail.com>
2023-05-05 14:49:43 +0000

    r5x: configs: Downgrade `Health` HAL to 2.0
    * Fixes battery capacity showing 50% for few seconds on boot/reboot.
    * Just Realmeme things
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -265,10 +265,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-service \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1.vendor
+    android.hardware.health@2.0-service
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
