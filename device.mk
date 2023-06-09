86ea6eb52997917400334ca2beb9fff015b3e613
Davide Garberi <dade.garberi@gmail.com>
2023-06-09 17:14:00 +0200

    r5x: Adjust libdisplayconfig targets
    * The old ones are deprecated
    
    Change-Id: I8c61a4cf46de2f34be68377f379ab50222994601

--- device.mk
+++ device.mk
@@ -172,9 +172,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack-service.example \
     gralloc.trinket \
     hwcomposer.trinket \
-    libdisplayconfig \
-    libdisplayconfig.vendor \
     libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
     libtinyxml \
     libtinyxml2 \
     libvulkan \
