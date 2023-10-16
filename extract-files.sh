e5d33294f7d5cc0cc628832faf9a3184606fd98e
Tim Zimmermann <tim@linux4.de>
2023-10-16 22:21:49 +0000

    r5x: Patch widevine to load libprotobuf-cpp-full-3.9.1
    * Patch widevine to load full variant as there's no lite vendorcompat variant
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,12 @@ function blob_fixup() {
             patchelf --replace-needed "libutils.so" "libutils-v30.so" "${2}"
             patchelf --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
