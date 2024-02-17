0a6692b8a53d7aef219c4dcf291e91069a0bf851
MAdMiZ <mizdrake7@gmail.com>
2024-02-17 15:43:59 +0000

    r5x: Let's try AfterLife
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023 The CherishOS Project
+# Copyright (C) 2023-2024 The Afterlife Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_r5x.mk
+    $(LOCAL_DIR)/afterlife_r5x.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_r5x-user \
-    lineage_r5x-userdebug \
-    lineage_r5x-eng
+    afterlife_r5x-user \
+    afterlife_r5x-userdebug \
+    afterlife_r5x-eng
--- afterlife_r5x.mk
+++ afterlife_r5x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2023 The LineageOS Project
+# Copyright (C) 2023-202 The Afterlife Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Sweet stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Halloween stuff
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -32,7 +32,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := r5x
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := lineage_r5x
+PRODUCT_NAME := afterlife_r5x
 PRODUCT_MODEL := Realme 5 Series
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
@@ -44,16 +44,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
     PRIVATE_BUILD_DESC="unknown-user 10 QKQ1.200209.002 unknown release-keys"
 
-# RisingOs Flags
-RISING_CHIPSET := SM6125
-RISING_MAINTAINER := MAdMiZ
-WITH_GMS := false
-RISING_PACKAGE_TYPE := Vanilla
-TARGET_CORE_GMS := true
-TARGET_CORE_GMS_EXTRAS := true
-TARGET_USE_GOOGLE_TELEPHONY := true
-TARGET_HAS_UDFPS := false
-TARGET_ENABLE_BLUR := true
-RISING_PACKAGE_TYPE := Gapps
-TARGET_EXCLUDES_AUDIOFX := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+# Afterlife Flags
+AFTERLIFE_GAPPS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+AFTERLIFE_CORE := true
+AFTERLIFE_EXTRA := true
+BUILD_GALLERYGO := true
+BUILD_GMAIL := false
+BUILD_GCALC := true
+AFTERLIFE_MAINTAINER := MAdMiZ
+
