e2d74990f653e89c19b10faef00f13770b9ba21d
MAdMiZ <mizdrake7@gmail.com>
2024-03-19 18:45:54 +0000

    Revert "r5x: Lets Rise Again"
    This reverts commit bb38e5cef34d1053ac7f926b718597c18bd796ac.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/rising_r5x.mk
+    $(LOCAL_DIR)/lineage_r5x.mk
 
 COMMON_LUNCH_CHOICES := \
-    rising_r5x-user \
-    rising_r5x-userdebug \
-    rising_r5x-eng
+    lineage_r5x-user \
+    lineage_r5x-userdebug \
+    lineage_r5x-eng
